433f6ab6d768509667ef6b8f87c8509f53d51afb
isa.mujahid <isa.mujahid@ext.ruma.co.id>
2019-09-19 15:02:16 +0700

    Add legal move function

--- Board.java
+++ Board.java
@@ -1,3 +1,6 @@
+import java.lang.Math;
+import java.util.Scanner;
+
 public class Board {
     final int width;
     final int height;
@@ -26,11 +29,38 @@ public class Board {
                 }
             }
         }
+
+        // Initialize White is going to move first
+        State.WTURN = true;
+    }
+
+    // Check if a move is legal or not
+    public boolean IsLegalMove(int current, int move){
+        // First, we're going to get 
+        int x = current/10;
+        int y = current%10;
+        int x1 = move/10;
+        int y1 = move%10;
+        // Check if the x and y index is not out of bound        
+        if ((x>=0 && x<=7)&&(y>=0 && y<=7) &&
+            (x1>=0 && x1<=7)&&(y1>=0 && y1<=7)) {
+            // Check if the player is choosing the right pawn
+            if (((board[x][y] == State.WHITE || board[x][y] == State.WKING) && State.WTURN) ||
+                ((board[x][y] == State.BLACK || board[x][y] == State.BKING) && !State.WTURN)) {
+                // Check whether the move state is empty or not
+                if (board[x1][y1] == State.NOPAWN) {
+                    if (Math.abs(x1-x) == 1 && Math.abs(y1-y) == 1) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     // Show the current checkers board
     public void ShowBoard(){
-        for (int i=0; i<height; i++) {
+        for (int i=height-1; i>=0; i--) {
             for (int j=0; j<width; j++) {
                 System.out.print(" "+board[i][j]+" ");
             }
@@ -42,5 +72,11 @@ public class Board {
     public static void main(String args[]) {
         Board b = new Board();
         b.ShowBoard();
+        int current;
+        int move;
+        Scanner inp = new Scanner(System.in);
+        current = inp.nextInt();
+        move = inp.nextInt();
+        System.out.println(b.IsLegalMove(current, move));
     }
 }
