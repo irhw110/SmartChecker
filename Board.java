9a3db0404fcee15ed3b9a3b4dd6bee132d73ec61
isa.mujahid <isa.mujahid@ext.ruma.co.id>
2019-09-19 14:06:25 +0700

    Add initial state of board

--- Board.java
+++ Board.java
@@ -1,19 +1,46 @@
-public class Board{
-    private int width;
-    private int height;
+public class Board {
+    final int width;
+    final int height;
     private int board[][];
 
-    public Board(int width, int height){
-        this.width = width;
-        this.height = height;
-
+    // Constructor for 8x8 board
+    public Board() {
+        width = 8;
+        height = 8;
         board = new int[height][width];
-        for (int i=0; i<height; i++){
-            for (int j=0; j< width; j++) {
-                board[i][j] = State.NOPAWN;
+
+        // Initialize all pawns on the board
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                // White Pawns
+                if ((i == 0 || i == 2) && (j % 2 == 0) || 
+                    (i == 1 && j % 2 == 1)) {
+                    board[i][j] = State.WHITE;
+                // Black Pawns
+                } else
+                if ((i == 5 || i == 7) && (j % 2 == 1) ||
+                    (i == 6 && j % 2 == 0)) {
+                    board[i][j] = State.BLACK;
+                } else {
+                    board[i][j] = State.NOPAWN;
+                }
             }
         }
     }
-    
-    
+
+    // Show the current checkers board
+    public void ShowBoard(){
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                System.out.print(" "+board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Driver
+    public static void main(String args[]) {
+        Board b = new Board();
+        b.ShowBoard();
+    }
 }
