ca03c326b6995a815d3dc0037780c524923f6f5b
isa.mujahid <isa.mujahid@ext.ruma.co.id>
2019-09-19 15:02:40 +0700

    Switch black & white representation

--- State.java
+++ State.java
@@ -1,8 +1,8 @@
 class State{
     static final int NOPAWN = 0;
-    static final int BLACK = 1;
-    static final int BKING = 2;
-    static final int WHITE = 3;
-    static final int WKING = 4;
+    static final int WHITE = 1;
+    static final int WKING = 2;
+    static final int BLACK = 3;
+    static final int BKING = 4;
     static boolean WTURN;
 }
