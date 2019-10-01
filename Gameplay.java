b75fc9e1f0d895f3233463ea121c27e38fd2ada9
imdar1 <isamujahid.im@gmail.com>
2019-10-01 23:25:25 +0700

    Update gameplay can select player type

--- Gameplay.java
+++ Gameplay.java
@@ -2,40 +2,91 @@ import java.util.Scanner;
 
 public class Gameplay {
     public static void main(String args[]) throws InterruptedException {
+        Scanner inp = new Scanner(System.in);
+        int player1;
+        int player2;
+
+        // Player 1 configuration
+        do {
+            System.out.println("Player 1 as:\n1. Human\n2. AI\n3. Random");
+            player1 = inp.nextInt();
+        } while (player1<1 || player1>3);
+
+        // Player 2 configuration
+        do {
+            System.out.println("Player 2 as:\n1. Human\n2. AI\n3. Random");
+            player2 = inp.nextInt();
+        } while (player2<1 || player2>3);
+
         Board b = new Board();
-        RandomBot rb = new RandomBot();
         b.showBoard();
         
         UI ui = new UI();
         ui.updateUI(b);
+        // Thread.sleep(500);
         while (b.isEndGame() == 0) {
 
-            // White turn, player
-            System.out.println("Giliran sekarang: "+b.getWTURN());
-            Scanner inp = new Scanner(System.in);
-            int currentX, currentY, moveX, moveY;
-            do {
-                currentX = inp.nextInt();
-                currentY = inp.nextInt();
-                moveX = inp.nextInt();
-                moveY = inp.nextInt();
-            } while (!b.isLegalMove(currentX, currentY, moveX, moveY));
-            b.movePawn(currentX, currentY, moveX, moveY);
+            // Player 1 turn
+            switch (player1) {
+                case 1:
+                    int currentX, currentY;
+                    int moveX, moveY;
+                    do {
+                        currentX = inp.nextInt();
+                        currentY = inp.nextInt();
+                        moveX = inp.nextInt();
+                        moveY = inp.nextInt();
+                    } while (!b.isLegalMove(currentX, currentY, moveX, moveY));
+                    b.movePawn(currentX, currentY, moveX, moveY);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    RandomBot rb = new RandomBot();
+                    rb.moveRandomly(b);
+                    break;
+            }
             b.showBoard();
             ui.updateUI(b);
-            Thread.sleep(500);
-            if (b.isEndGame() != 0) break;            
+            // Thread.sleep(500);
+            if (b.isEndGame() != 0) break;  
 
-            //Black turn, random
-            rb.moveRandomly(b);
-            System.out.println("Giliran sekarang: "+b.getWTURN());
-            b.showBoard();
+            // Player 2 turn
+            switch (player2) {
+                case 1:
+                    int currentX, currentY;
+                    int moveX, moveY;
+                    do {
+                        currentX = inp.nextInt();
+                        currentY = inp.nextInt();
+                        moveX = inp.nextInt();
+                        moveY = inp.nextInt();
+                    } while (!b.isLegalMove(currentX, currentY, moveX, moveY));
+                    b.movePawn(currentX, currentY, moveX, moveY);
+                    break;
+                case 2:
+                    AlphaBeta ab = new AlphaBeta();
+                    ChosenMove ctemp= new ChosenMove(0,0,0);
+                    ctemp.CopyChosenMove(ab.alphabeta(b));
+                
+                    int[] move = RandomBot.moveTo(b.getBlackPawns().getXFrom(ctemp.idpawn),b.getBlackPawns().getYFrom(ctemp.idpawn),ctemp.move);
+                    b.movePawn(b.getBlackPawns().getXFrom(ctemp.idpawn),b.getBlackPawns().getYFrom(ctemp.idpawn), move[0], move[1]);
+        
+                    break;
+                case 3:
+                    RandomBot rb = new RandomBot();
+                    rb.moveRandomly(b);
+                    break;
+            }
             ui.updateUI(b);
+            // Thread.sleep(500);
+
         }
         if (b.isEndGame() == 1) {
             System.out.println("Putih menang gan");
         } else {
             System.out.println("Hitam menang gan");
         }
+        inp.close();
     }
 }
